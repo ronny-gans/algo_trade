19a0e64f739088a52597a0b297c57d0928be0970
Ronny Ardiansyah <ronnyardiansya@gmail.com>
2026-05-03 22:20:01 +0700

    feat: backtest FINAL - RR 3:1 single pullback PF 1.09 P(profit) 99.88%

--- strategies/backtest.py
+++ strategies/backtest.py
@@ -48,7 +48,7 @@ def run_backtest():
     times_h1 = [bar[0] for bar in rates_h1]
     atr_full = indicators.indicator.ATR(14).calculate(closes_m15,highs_m15,lows_m15)
     tema_60_h1 = indicators.indicator.TEMA(60).calculate(closes_h1)
-
+    adx = indicators.indicator.ADX(14).calculate(closes_m15,highs_m15,lows_m15)
     print("calculating indicators")
     # using existing classes
     tema9_full = indicators.indicator.TEMA(9).calculate(closes_m15)
@@ -76,6 +76,10 @@ def run_backtest():
     rsi_diff = len(rsi_full) - len(tema60_full)
     # atr diff
     atr_diff = len(atr_full) - len(tema60_full)
+    atr_aligned = atr_full[atr_diff:]
+    # adx aligned 
+    adx_diff = len(adx) - len(tema60_full)
+    adx_aligned = adx[adx_diff:]
     hours =BacktestHours()
     trade_returns =[]
     
@@ -107,72 +111,79 @@ def run_backtest():
         tema_idx = htf_idx-h1_offset
         if tema_idx < 0 or tema_idx >= len(tema_60_h1):
             continue
-        trend_up = closes_h1[htf_idx] > tema_60_h1[tema_idx]
-        trend_down = closes_h1[htf_idx] < tema_60_h1[tema_idx]
+        if htf_idx < 1 or tema_idx < 1:
+            continue
+        trend_up = closes_h1[htf_idx-1] > tema_60_h1[tema_idx-1]
+        trend_down = closes_h1[htf_idx-1] < tema_60_h1[tema_idx-1]
+        
         # crossover logic
         # golden cross
         #if tema9_aligned[i-1] <= tema20_aligned[i-1] and tema9_aligned[i] > tema20_aligned[i]:
-        if trend_up:
+        if trend_up: #and adx_aligned[i]>=0.25:
             trend_up_count+=1
             if tema60_full[i] < closes_m15[idx]: 
-                if closes_m15[idx] < closes_m15[idx-1]: #pullback
-                    crosses_found+=1
-                    #rsi_idx = i+rsi_diff
-                    #if 0 <= rsi_idx < len(rsi_full) and rsi_full[rsi_idx] < 45:
-                    if True:
-                        entry = closes_m15[idx]
-                        atr_idx = i +atr_diff
-                        atr = atr_full[atr_idx]
-                        sl = atr*1.5 #stoploss 1.5x ATR
-                        tp = sl *2 # TP at 2 times SL
-                        result = None 
-                        for j in range (idx+1, min(idx+48, len(closes_m15)-1)): # max 48 bars = 12 hour
-                            high_j = highs_m15[j]
-                            low_j = lows_m15[j]
-                            if high_j >= entry + tp:
-                                result = tp / entry    # TP hit 
-                                break
-                            if low_j <= entry - sl:
-                                result = -(sl / entry) # SL hit 
-                                break
-                        if result is None:
-                            exit_idx = min(idx+48, len(closes_m15)-1)
-                            result = (closes_m15[exit_idx]-entry)/entry # time stop
-                            time_stop_hitted+=1
-                        trade_returns.append(result-0.0001) # spread
+                #if tema60_full[i] < tema20_aligned[i] and tema20_aligned[i] < tema9_aligned[i]: 
+                    if closes_m15[idx] < closes_m15[idx-1]: #pullback
+                        crosses_found+=1
+                        #rsi_idx = i+rsi_diff
+                        #if 0 <= rsi_idx < len(rsi_full) and rsi_full[rsi_idx] < 45:
+                        if True:
+                            entry = closes_m15[idx+1]
+                            atr = atr_aligned[i]
+                            sl = atr*1.5 #stoploss 1.5x ATR
+                            tp = sl *3 # TP at 2 times SL
+                            result = None 
+                            for j in range (idx+2,min(idx+48, len(closes_m15)-1)): # max 48 bars = 12 hour
+                                high_j = highs_m15[j]
+                                low_j = lows_m15[j]
+                                if high_j >= entry + tp:
+                                    result = tp / entry    # TP hit 
+                                    break
+                                if low_j <= entry - sl:
+                                    result = -(sl / entry) # SL hit 
+                                    break
+                            if result is None:
+                                exit_idx = min(idx+48, len(closes_m15)-1)
+                                result = (closes_m15[exit_idx]-entry)/entry # time stop
+                                time_stop_hitted+=1
+                            spread = random.uniform(0.00005, 0.0002)
+                            slippage = random.uniform(0, 0.0001)
+                            trade_returns.append(result-spread-slippage) # spread
                     #else:
                         #rsi_fail+=1
         # dead cross
         #elif tema9_aligned[i-1] >= tema20_aligned[i-1] and tema9_aligned[i] < tema20_aligned[i]:
-        elif trend_down:
+        elif trend_down: #and adx_aligned[i]>=0.25:
             trend_down_count+=1
             if tema60_full[i] > closes_m15[idx]: # 
-                if closes_m15[idx] > closes_m15 [idx-1]: #pullback
-                    crosses_found += 1
-                    rsi_idx = i + rsi_diff
-                    #if 0 <= rsi_idx < len(rsi_full) and rsi_full[rsi_idx] > 55: 
-                    if True:
-                    # Short: Entry - Exit
-                        entry = closes_m15[idx]
-                        atr_idx = i +atr_diff
-                        atr = atr_full[atr_idx]
-                        sl = atr*1.5 #stoploss 1.5x ATR
-                        tp = sl *2 # TP at 2 times SL
-                        result = None 
-                        for j in range (idx+1, min(idx+48, len(closes_m15)-1)): # max 48 bars = 12 hour
-                            high_j = highs_m15[j]
-                            low_j = lows_m15[j]
-                            if low_j <= entry - tp:
-                                result = tp / entry    # TP hit 
-                                break
-                            if high_j >= entry + sl:
-                                result = -(sl / entry) # SL hit 
-                                break
-                        if result is None:
-                            exit_idx = min(idx+48, len(closes_m15)-1)
-                            result = (entry-closes_m15[exit_idx])/entry # time stop
-                            time_stop_hitted+=1
-                        trade_returns.append(result-0.0001) # spread
+                #if tema60_full[i] > tema20_aligned[i] and tema20_aligned[i] > tema9_aligned[i]:
+                    if closes_m15[idx] > closes_m15 [idx-1]: #pullback
+                        crosses_found += 1
+                        #rsi_idx = i + rsi_diff
+                        #if 0 <= rsi_idx < len(rsi_full) and rsi_full[rsi_idx] > 55: 
+                        if True:
+                        # Short: Entry - Exit
+                            entry = closes_m15[idx+1]
+                            atr = atr_aligned[i]
+                            sl = atr*1.5 #stoploss 1.5x ATR
+                            tp = sl *3 # TP at 2 times SL
+                            result = None 
+                            for j in range (idx+2, min(idx+48, len(closes_m15)-1)): # max 48 bars = 12 hour
+                                high_j = highs_m15[j]
+                                low_j = lows_m15[j]
+                                if low_j <= entry - tp:
+                                    result = tp / entry    # TP hit 
+                                    break
+                                if high_j >= entry + sl:
+                                    result = -(sl / entry) # SL hit 
+                                    break
+                            if result is None:
+                                exit_idx = min(idx+48, len(closes_m15)-1)
+                                result = (entry-closes_m15[exit_idx])/entry # time stop
+                                time_stop_hitted+=1
+                            spread = random.uniform(0.00005, 0.0002)
+                            slippage = random.uniform(0, 0.0001)
+                            trade_returns.append(result-spread-slippage) # spread
                     #else:
                         #rsi_fail+=1
     print(f"Debug: Total crosses found           : {crosses_found}")
@@ -213,10 +224,11 @@ def run_monte_carlo(returns, num_sims=100000, start_bal=10000):
     # this loops will be the slowest part without numpy
     for _ in range(num_sims):
         current_balances = start_bal
+        shuffled = returns[:]
+        random.shuffle(shuffled)
         # simulate random sequence of the trades we found
-        for _ in range(num_trades):
+        for r in shuffled:
             # random.choice picks one trade return from our list
-            r=random.choice(returns)
             current_balances*=(1+r)
         final_balances.append(current_balances)
     # -- Manual statistics--
---
+++ strategies/backtest_result.md
@@ -0,0 +1,230 @@
+📊 BACKTEST RESULTS SUMMARY (TEMA aligned)
+
+EURUSD M15 | H1 Trend Filter
+
+Date: May 2026
+
+================================
+
+
+
+✅ FINAL VERSION (No RSI Filter)
+
+Trades        : 5005
+
+Win Rate      : 40.32%
+
+Avg Win       : 0.0020 (0.20%)
+
+Avg Loss      : -0.0012 (-0.12%)
+
+Profit Factor : 1.08
+
+Time Stop Hit : 402
+
+
+
+💰 MONTE CARLO (100,000 sims)
+
+Start Balance : $10,000
+
+Median Final  : $13,348.54 (33%)
+
+P(Profit)     : 99.06%
+
+Max Balance   : $23060.99
+
+Min Balance   : $7511.05
+
+
+
+================================
+
+📝 STRATEGY LOGIC
+
+================================
+
+Timeframe : M15 entry, H1 trend
+
+Entry Long : H1 price > H1 TEMA60
+
+&#x20;            M15 price > M15 TEMA60
+
+&#x09;     **TEMA 20 M15 > TEMA 60 M15**
+
+&#x09;     **TEMA 9 M15 > TEMA 20 M15**
+
+&#x20;            M15 pullback (1 red candle)
+
+Entry Short: H1 price < H1 TEMA60
+
+&#x20;            M15 price < M15 TEMA60
+
+&#x09;     **TEMA 20 M15 < TEMA 60 M15**
+
+&#x09;     **TEMA 9 M15 < TEMA 20 M15**
+
+&#x20;            M15 pullback (1 green candle)
+
+SL         : ATR × 1.5
+
+TP         : SL × 2 (2:1 RR)
+
+Max hold   : 48 bars (12 hours)
+
+Session    : London 07-11 UTC
+
+&#x20;            NY 12-16 UTC
+
+
+
+
+
+📊 BACKTEST RESULTS SUMMARY (RR 3:1)
+
+EURUSD M15 | H1 Trend Filter
+
+Date: May 2026
+
+================================
+
+
+
+✅ FINAL VERSION (No RSI Filter)
+
+Trades        : 7740
+
+Win Rate      : 34.17%
+
+Avg Win       : 0.0025 (0.25%)
+
+Avg Loss      : -0.0012 (-0.12%)
+
+Profit Factor : 1.09
+
+Time Stop Hit : 1178
+
+
+
+💰 MONTE CARLO (100,000 sims)
+
+Start Balance : $10,000
+
+Median Final  : $17,098.73 (70%)
+
+P(Profit)     : 99.88%
+
+Max Balance   : $39,101.78
+
+Min Balance   : $8018.50
+
+
+
+================================
+
+📝 STRATEGY LOGIC
+
+================================
+
+Timeframe : M15 entry, H1 trend
+
+Entry Long : H1 price > H1 TEMA60
+
+&#x20;            M15 price > M15 TEMA60
+
+&#x20;            M15 pullback (1 red candle)
+
+Entry Short: H1 price < H1 TEMA60
+
+&#x20;            M15 price < M15 TEMA60
+
+&#x20;            M15 pullback (1 green candle)
+
+SL         : ATR × 1.5
+
+TP         : SL × 3 (3:1 RR)
+
+Max hold   : 48 bars (12 hours)
+
+Session    : London 07-11 UTC
+
+&#x20;            NY 12-16 UTC
+
+
+
+📊 BACKTEST RESULTS SUMMARY (Two candle pullback)
+
+EURUSD M15 | H1 Trend Filter
+
+Date: May 2026
+
+================================
+
+
+
+✅ FINAL VERSION (No RSI Filter)
+
+Trades        : 2406
+
+Win Rate      : 41.77%
+
+Avg Win       : 0.0019 (0.19%)
+
+Avg Loss      : -0.0012 (-0.12%)
+
+Profit Factor : 1.11
+
+Time Stop Hit : 215
+
+
+
+💰 MONTE CARLO (100,000 sims)
+
+Start Balance : $10,000
+
+Median Final  : $12,129.14 (21%)
+
+P(Profit)     : 98.69%
+
+Max Balance   : $17,989.27
+
+Min Balance   : $8305.81
+
+
+
+================================
+
+📝 STRATEGY LOGIC
+
+================================
+
+Timeframe : M15 entry, H1 trend
+
+Entry Long : H1 price > H1 TEMA60
+
+&#x20;            M15 price > M15 TEMA60
+
+&#x20;            M15 pullback (2 red candle)
+
+Entry Short: H1 price < H1 TEMA60
+
+&#x20;            M15 price < M15 TEMA60
+
+&#x20;            M15 pullback (2 green candle)
+
+SL         : ATR × 1.5
+
+TP         : SL × 2 (2:1 RR)
+
+Max hold   : 48 bars (12 hours)
+
+Session    : London 07-11 UTC
+
+&#x20;            NY 12-16 UTC
+
+
+
+
+
+
+
